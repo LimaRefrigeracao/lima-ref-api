f09834a0b0df8e623f0916cb79544dcd229af7c9
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2023-09-11 04:23:25 -0300

    update init

---
+++ src/controllers/servicesController.js
@@ -0,0 +1,10 @@
+const servicesModel = require('../models/servicesModel');
+
+const getAll = async (req, res) => {
+  const services = await servicesModel.getAll();
+  return res.status(200).json(services);
+};
+
+module.exports = {
+  getAll,
+};
---
+++ src/models/servicesModel.js
@@ -0,0 +1,10 @@
+const connection = require("./connection");
+
+const getAll = async () => {
+  const services = await connection.execute("SELECT * FROM services");
+  return services;
+};
+
+module.exports = {
+  getAll,
+};
