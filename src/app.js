31e60b132dd956a77967e101f98765c6fe942c44
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2023-09-15 14:56:17 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,15 @@ const swaggerDocument = require("../swagger.json");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(router);
 
 module.exports = app;
