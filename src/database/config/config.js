013d3027de01ab8b9161b9594102eac261ade8ab
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2023-09-25 06:46:21 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,17 +7,13 @@ module.exports = {
     dialectOptions: {
       bigNumberStrings: true,
     },
-    url: `${process.env.DATABASE_URL}`,
+    url: process.env.DATABASE_URL,
   },
   production: {
     dialect: "postgres",
     dialectOptions: {
       bigNumberStrings: true,
     },
-    username: "limaref_26w1_user",
-    password: "TOYnABMXqIGQuySmzwUFgw4kDNW03Ywg",
-    database: "limaref_26w1",
-    host: "dpg-ck8knmnsasqs739mdp9g-a",
-    port: 5432,
+    url: process.env.DATABASE_URL,
   },
-}; 
+} 
